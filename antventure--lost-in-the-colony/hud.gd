70769e8503f37f576f0f799acf10a069f1a4da0a
Renae Hunt <jerseeh2003@gmail.com>
2025-10-05 10:18:25 -0600

    Pause Functionality Finished
    Added the ability to pause
    - credit scene added but not finished

--- antventure--lost-in-the-colony/hud.gd
+++ antventure--lost-in-the-colony/hud.gd
@@ -1,10 +1,11 @@
 # The HUD should be used for anything that needs to be on screen at all times i.e. score,
 # time, lives, etc.
-
 extends Control
+
+@onready var pause: Control = $PAUSE
+
 @onready var score_label: Label = $ScoreLabel
-
-
+var is_paused:= false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	# updates should be done by functions and grouped together
@@ -17,3 +18,14 @@ func _process(delta: float) -> void:
 # here. 
 func update_labels():
 	score_label.text = str("Score: " , GvPlayer.score)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		if is_paused:
+			pause.hide()
+			is_paused = false
+			get_tree().paused = false
+		elif !is_paused:
+			pause.show()
+			is_paused = true
+			get_tree().paused = true
--- antventure--lost-in-the-colony/main_menu.gd
+++ antventure--lost-in-the-colony/main_menu.gd
@@ -1,23 +1,15 @@
 extends Control
 
+@onready var credits: ColorRect = $Credits
+
 @onready var playgroundLevel := "res://test_main/playground_game.tscn"
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 
 func _on_play_button_pressed() -> void:
 	print("Please add level node!")
 
 
 func _on_credit_buttons_pressed() -> void:
-	pass # Replace with function body.
-
+	credits.show()
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
@@ -25,3 +17,7 @@ func _on_exit_button_pressed() -> void:
 
 func _on_button_pressed() -> void:
 	Loading.load_scene(playgroundLevel, true)
+
+
+func _on_return_button_pressed() -> void:
+	credits.hide()
