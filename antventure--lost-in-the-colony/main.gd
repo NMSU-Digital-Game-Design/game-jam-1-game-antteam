902454755381eaa2423f24ab353c7c86b8726780
VictoriaTrev <vrtrevin@nmsu.edu>
2025-10-05 01:46:13 -0600

    updates from merge

--- antventure--lost-in-the-colony/main.gd
+++ antventure--lost-in-the-colony/main.gd
@@ -1,18 +1,5 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_player_out_of_lives() -> void:
-	get_tree().quit()
 @onready var loading: CanvasLayer = $loading
 
 @onready var mainMenu = "res://mainmenu/main_menu.tscn"
