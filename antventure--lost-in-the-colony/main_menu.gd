8e40d4d1f451ab824db0a203828ec9a906704543
Renae Hunt <jerseeh2003@gmail.com>
2025-10-05 16:01:52 -0600

    fixed bug after merging

--- antventure--lost-in-the-colony/main_menu.gd
+++ antventure--lost-in-the-colony/main_menu.gd
@@ -3,16 +3,7 @@ extends Control
 @onready var credits: ColorRect = $Credits
 @onready var playgroundLevel := "res://test_main/playground_game.tscn"
 @onready var level_loader := "res://level_loader.tscn"
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-@onready var playgroundLevel := "res://test_main/playground_game.tscn"
 
 func _on_play_button_pressed() -> void:
 	Loading.load_scene(level_loader, true)
